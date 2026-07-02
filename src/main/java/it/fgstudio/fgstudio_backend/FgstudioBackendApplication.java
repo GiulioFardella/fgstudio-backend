8de5897dd1485b18cfb8b4441e1c21fef9700943
GiulioFardella <giulio.fardella@outlook.it>
2026-07-02 11:36:30 +0200

    Initial commit

---
+++ src/main/java/it/fgstudio/fgstudio_backend/FgstudioBackendApplication.java
@@ -0,0 +1,13 @@
+package it.fgstudio.fgstudio_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FgstudioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FgstudioBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/fgstudio/fgstudio_backend/FgstudioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package it.fgstudio.fgstudio_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FgstudioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
